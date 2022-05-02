4ed4da13402fa88c318ccb4fef6cf4cd3b4b6fd6
stanko <stanko@STAN-LAPTOP>
2022-05-02 16:05:14 +0300

    Add project files.

---
+++ ASP.NET Students Database Example/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ASP.NET_Students_Database_Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_Students_Database_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Student> students = new List<Student>();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowPicture()
+        {
+
+            return View();
+        }
+
+        public ActionResult ShowTable()
+        {
+            students = GetAllStudentsSortedByGrade();
+            return View(students);
+        }
+
+        private List<Student> GetAllStudents()
+        {
+            using (StudentsDBEntities sdbe = new StudentsDBEntities())
+            {
+                return sdbe.Students.ToList();
+            }
+        }
+
+        private List<Student> GetAllStudentsSortedByGrade()
+        {
+            using (StudentsDBEntities sdbe = new StudentsDBEntities())
+            {
+                return sdbe.Students.OrderByDescending(x => x.Grade).ToList();
+            }
+        }
+    }
+}
---
+++ ASP.NET Students Database Example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_Students_Database_Example.MvcApplication" Language="C#" %>
